9e6dd51a4fb584c7037a224c2c2308cae78fd51a
kalinDev <kalin.bussiness@gmail.com>
2023-07-04 00:13:42 +0300

    Added rendering of pug template and updated css styles

--- routes/shop.js
+++ routes/shop.js
@@ -3,9 +3,13 @@ const express = require("express");
 
 const rootDir = require("../util/path");
 const router = express.Router();
+const adminData = require("./admin");
 
 router.get("/", (req, resp, next) => {
-  resp.sendFile(path.join(rootDir, "views", "shop.html"));
+  const products = adminData.products;
+  console.log("shop.js", adminData.products);
+  resp.render("shop", { prods: products, docTitle: "Shop" });
+  // resp.sendFile(path.join(rootDir, "views", "shop.html"));
 });
 
 router.get("/users", (req, resp, next) => {
