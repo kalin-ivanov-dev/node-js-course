a0c731cef32a59bf4c3f861de2f87f21e3850e04
kalinDev <kalin.bussiness@gmail.com>
2023-07-07 00:03:33 +0300

    Added logic for writing/saving product data  and reading from a file in the product model

--- controllers/products.js
+++ controllers/products.js
@@ -18,13 +18,14 @@ exports.postAddProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  const products = Product.getAll();
-  res.render("shop", {
-    prods: products,
-    pageTitle: "Shop",
-    path: "/",
-    hasProducts: products.length > 0,
-    activeShop: true,
-    productCSS: true,
+  Product.getAll((products) => {
+    res.render("shop", {
+      prods: products,
+      pageTitle: "Shop",
+      path: "/",
+      hasProducts: products.length > 0,
+      activeShop: true,
+      productCSS: true,
+    });
   });
 };
--- models/products.js
+++ models/products.js
@@ -1,14 +1,43 @@
-const products = [];
+const { log } = require("console");
+const fs = require("fs");
+const path = require("path");
+
+const p = path.join(
+  path.dirname(process.mainModule.filename),
+  "data",
+  "products.json"
+);
+
 module.exports = class Product {
   constructor(title) {
     this.title = title;
   }
 
   save() {
-    products.push(this);
+    fs.readFile(p, (err, fileContent) => {
+      let products = [];
+
+      if (!err) {
+        products = JSON.parse(fileContent);
+      }
+
+      // scope context sensitive
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), (err) => {
+        console.log("-------THERE WAS AN ERROR ------- ");
+        console.log(p);
+        console.log(err);
+      });
+    });
   }
 
-  static getAll() {
-    return products;
+  static getAll(cb) {
+    fs.readFile(p, (err, fileContent) => {
+      let products = [];
+      if (err) {
+        cb([]);
+      }
+      cb(JSON.parse(fileContent));
+    });
   }
 };
