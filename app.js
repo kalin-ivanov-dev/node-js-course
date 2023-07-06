599baf26582f0aa273b071c45e032314e455e3ea
kalinDev <kalin.bussiness@gmail.com>
2023-07-06 23:02:32 +0300

    Added error (404 not found) controller

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ app.set("views", "views");
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
+const errorController = require("./controllers/error");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
@@ -16,8 +17,6 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, resp, next) => {
-  resp.status(404).render("404", { pageTitle: "Page not found" });
-});
+app.use(errorController.get404);
 
 app.listen(3000);
