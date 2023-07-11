633630ce7b6d3b1efe0a0f618ec9007469f9641f
kalinDev <kalin.bussiness@gmail.com>
2023-07-11 23:19:17 +0300

    Added helper functions in product model

--- models/products.js
+++ models/products.js
@@ -8,36 +8,33 @@ const p = path.join(
   "products.json"
 );
 
+const getProductsFromFile = (cb) => {
+  fs.readFile(p, (err, fileContent) => {
+    let products = [];
+    if (err) {
+      return cb([]);
+    }
+    cb(JSON.parse(fileContent));
+  });
+};
+
 module.exports = class Product {
   constructor(title) {
     this.title = title;
   }
 
   save() {
-    fs.readFile(p, (err, fileContent) => {
-      let products = [];
-
-      if (!err) {
-        products = JSON.parse(fileContent);
-      }
-
+    getProductsFromFile((products) => {
       // scope context sensitive
       products.push(this);
       fs.writeFile(p, JSON.stringify(products), (err) => {
         console.log("-------THERE WAS AN ERROR ------- ");
-        console.log(p);
         console.log(err);
       });
     });
   }
 
   static getAll(cb) {
-    fs.readFile(p, (err, fileContent) => {
-      let products = [];
-      if (err) {
-        cb([]);
-      }
-      cb(JSON.parse(fileContent));
-    });
+    getProductsFromFile(cb);
   }
 };
